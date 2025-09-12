9ed4bb1b5c0103680a0ca0a104309428c2143f7f
Jacob Pach <jpach@mtech.edu>
2025-09-12 07:13:24 -0600

    add tests/

---
+++ tests/tests.c
@@ -0,0 +1,26 @@
+//#include <assert.h>   // commented out, not needed
+#include "unity.h"
+
+int result; // global variable for test results
+
+// ----------------- UNITY SETUP / TEARDOWN -----------------
+void setUp() 
+{  
+
+}
+
+void tearDown() 
+{
+
+}
+
+// ----------------- TEST FUNCTIONS -----------------
+// ...
+
+// ----------------- RUN ALL TESTS -----------------
+int main(void) 
+{
+    UNITY_BEGIN();
+    //...
+    return UNITY_END();
+}
