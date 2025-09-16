7034fa307d4b20711545aabf1f4ed0f022001ecb
jmortensenmtech <jmortensen@mtech.edu>
2025-09-16 16:25:28 -0600

    Added code2.c math functions to math_ops.

--- src/math_ops.c
+++ src/math_ops.c
@@ -11,3 +11,22 @@ float add(float x, float y)
     return x + y;
 }
 
+float square(float x) 
+{
+    return x * x;
+}
+
+float cube(float x) 
+{
+    return x * x * x;
+}
+
+float pow(float base, int exponent ) 
+{
+    float res = 1;
+    for(int i = 0; i < exponent; i++) 
+    {
+        res *= base;
+    }
+    return res;
+}
