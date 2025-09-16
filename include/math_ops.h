de7a81d840897d42056da0dd761a4c306f24079e
jmortensenmtech <jmortensen@mtech.edu>
2025-09-16 17:56:56 -0600

    Created header for math_ops.

---
+++ include/math_ops.h
@@ -0,0 +1,18 @@
+#ifndef MATH_OPS_H
+#define MATH_OPS_H
+
+// Function Declarations
+
+float square(float x);
+
+float cube(float x);
+
+float pow(float base, int exponent);
+
+float multiply(float x, float y);
+
+float add(float x, float y);
+
+float sub(float a, float b);
+
+#endif 
