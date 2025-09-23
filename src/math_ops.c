a38f4627dc04118ee9ef1965410a87d3ffd0cd8f
jmortensenmtech <jmortensen@mtech.edu>
2025-09-23 17:43:39 -0600

    Restored sin function in math_ops.c

--- src/math_ops.c
+++ src/math_ops.c
@@ -50,21 +50,17 @@ float divide(float a, float b)
 
 float sin(float a, int steps)
 {
-    return 1.0;  
-    // change applied
-}
-
-float cos(float a, int steps)
-{
     //hardcode steps and convert a into radians
     steps = 10;
     a = a * 3.14159265358979323846f / 180.0f;
 
     float result = 0.0f;
-    float term = 1.0f;
+    float term = a;
+
+    result += term;
 
     for (int i = 1; i < steps; ++i) {
-        int power = 2 * i;
+        int power = 2 * i + 1;
         term *= -a * a / ((power - 1) * power);
         result += term;
     }
